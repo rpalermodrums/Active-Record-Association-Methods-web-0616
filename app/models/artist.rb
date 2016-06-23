ae7fa052a7275cb05708912be27d7fb3ac885f18
Ryan Palermo <stix250@gmail.com>
2016-06-22 20:39:43 -0400

    Done.

--- app/models/artist.rb
+++ app/models/artist.rb
@@ -4,13 +4,16 @@ class Artist < ActiveRecord::Base
 
   def get_genre_of_first_song
     #return the genre of the artist's first saved song
+    self.songs.first.genre
   end
 
   def song_count
     #return the number of songs associated with the artist
+    self.songs.length
   end
 
   def genre_count
     #return the number of genres associated with the artist
+    Song.all.select {|song| song.artist_id == self.id}.map(&:genre_id).uniq.count
   end
 end
--- app/models/genre.rb
+++ app/models/genre.rb
@@ -4,13 +4,16 @@ class Genre < ActiveRecord::Base
 
   def song_count
     # return the number of songs in a genre
+    self.songs.count
   end
 
   def artist_count
     # return the number of artists associated with the genre
+    Song.select{|song| song.genre_id == self.id}.map(&:artist).uniq.count
   end
 
   def all_artist_names
     # return an array of strings containing every musician's name
+    Song.select{|song| song.genre_id == self.id}.map(&:artist).map(&:name).uniq
   end
 end
